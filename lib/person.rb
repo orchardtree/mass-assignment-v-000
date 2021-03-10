cb01493b8c55090dcebc90bf1155d1214bca8165
Jesse Wenger <wengerj@gmail.com>
2021-03-10 22:34:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
-  attr_accessor :name, :birthday
+  attr_accessor :name, :birthday, hair
   def initialize(attributes)
     attributes.each{|key, value| self.send(("#{key}="), value)}
   end
